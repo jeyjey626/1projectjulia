ae5a6aef07c3cd92d8fefd6e751657d941c10a91
jeyjey626 <juliamakulec@gmail.com>
2019-03-06 19:35:30 +0100

    Ok biblioteka daty

--- src/com/Examination.java
+++ src/com/Examination.java
@@ -1,8 +1,10 @@
 package com;
 
+import java.util.Date;
+
 public class Examination {
 
-    private Date date; //todo: biblioteka gdzie jest data
+    private Date date;
     private double mass;
     private int height;
     private double bmi;
@@ -15,23 +17,13 @@ public class Examination {
         this.bmi = mass / (height/100)*(height/100); //todo: Zdecydować czy to powinno być tutaj?? Czy są dobre typy zmiennych
     }
 
-    public void setDate(Date date) {
-        this.date = date;
-    }
+    public void setDate(Date date) { this.date = date; }
 
-    public Date getDate() {
-        return date;
-    }
+    public Date getDate() { return date; }
 
-    public double getBmi() {
-        return bmi;
-    }
+    public double getBmi() { return bmi; }
 
-    public double getMass() {
-        return mass;
-    }
+    public double getMass() { return mass; }
 
-    public int getHeight() {
-        return height;
-    }
+    public int getHeight() { return height; }
 }
