3669c97c432c91f0312623dc9486feceb7c3cf1e
jeyjey626 <juliamakulec@gmail.com>
2019-03-25 14:36:30 +0100

    PESEL check done

--- src/com/PatientPresenter.java
+++ src/com/PatientPresenter.java
@@ -1,20 +1,36 @@
 package com;
 
 
+
+import org.apache.commons.lang3.StringUtils;
+import java.util.Date;
+
 public class PatientPresenter {
 
 
     private Patient patient;
 
-    public PatientPresenter(Patient patient)
+    public PatientPresenter()
     {
-        this.patient = patient;
+
     }
 
-    public void savePButton(Patient patient){
-
+    public int savePButton(String name, String surname, String pesel, boolean sex, String insurance){
+        int check = checkP(pesel);
+        if(check == 0){
+            Patient patient = new Patient(name, surname, pesel, sex, insurance);
+            //TODO: send patient to library (here or in Patient class?)
+        }
+        return check;
     };
 
-    //TODO: here all the check methods for data and button action
+    private int checkP(String pesel){
+        int check = 0;
+        if(pesel.length() != 11) {check = 1;}
+        if(!StringUtils.isNumeric(pesel)){check = 2;}
+        return check;
+
+
+    }
     //TODO: checkout delegating stuff
 }
