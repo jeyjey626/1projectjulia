681b7f2dd6d0736a4b1cec980e03bbc4bd3cc15f
jeyjey626 <juliamakulec@gmail.com>
2019-03-29 14:15:36 +0100

    changed abort exam function, bug fixes, code clearance

--- src/com/GUI.java
+++ src/com/GUI.java
@@ -9,7 +9,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.jdatepicker.impl.JDatePanelImpl;
 import org.jdatepicker.impl.JDatePickerImpl;
 import org.jdatepicker.impl.UtilDateModel;
-
 import javax.swing.*;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
@@ -21,7 +20,6 @@ import javax.swing.event.DocumentListener;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.KeyEvent;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Properties;
@@ -30,7 +28,6 @@ import java.util.Date;
 
 //checklist
 //todo layout in exam and patient panels
-//todo decide if abort button in exam should clear all or only exam
 //todo comment your code!
 
 public class GUI extends JFrame{
@@ -38,24 +35,15 @@ public class GUI extends JFrame{
 
     //Declarations
     private Presenter presenter;
-
-    private JPanel examPanel;
-    private JPanel patientPanel;
-
-    private JTable patientTable;
+    private JPanel examPanel, patientPanel;
     private JFrame frame;
-
-    private JButton savePatientButton;
-    private JTextField nameT, surnameT, peselT;
+    private JTable patientTable;
+    private JButton savePatientButton, deletePatientButton;
+    private JTextField nameT, surnameT, peselT, weightT, bmiT, heightT;
     private JComboBox<String> iBox;
     private JRadioButton male, female;
-
-    private JTextField weightT, bmiT, heightT;
     private JDatePickerImpl datePicker;
-    private JButton deletePatientButton;
-
     private int yearInit, monthInit, dayInit;
-
     private static final int TEXTFIELDCOL = 15;
 
     private GUI()
@@ -65,15 +53,14 @@ public class GUI extends JFrame{
     private void initUI()
     {
 
-        presenter = new Presenter();
-        frame = new JFrame ("Rejestracja Wyników Badań");
+        presenter = new Presenter();//initializing presenter(MVP model)
 
+        frame = new JFrame ("Rejestracja Wyników Badań");
         frame.setVisible(true);
 
+        //------------------------- menu bar -----------------------------------
         JMenuBar menuBar = new JMenuBar();
         JMenu menu = new JMenu("Aplikacja");
-        menu.setMnemonic(KeyEvent.VK_A);
-
         JMenuItem closeApp = new JMenuItem("Zamknij ALT+F4");
         closeApp.addActionListener(e -> frame.dispose());
         menu.add(closeApp);
@@ -87,75 +74,37 @@ public class GUI extends JFrame{
         JPanel nameCnt = new JPanel();
         JLabel nameL = new JLabel("Imię:", SwingConstants.LEFT);
         nameT = new JTextField(TEXTFIELDCOL);
-        nameCnt.add(nameL);
-        nameCnt.add(nameT);
-
+        nameCnt.add(nameL); nameCnt.add(nameT);
         //------------------------ Surname ------------------------
         JPanel surnameCnt = new JPanel();
         JLabel surnameL = new JLabel("Nazwisko:", SwingConstants.LEFT);
         surnameT = new JTextField(TEXTFIELDCOL);
-        surnameCnt.add(surnameL);
-        surnameCnt.add(surnameT);
-
+        surnameCnt.add(surnameL); surnameCnt.add(surnameT);
         //------------------------ PESEL ------------------------
         JPanel peselCnt = new JPanel();
         JLabel peselL = new JLabel("PESEL:", SwingConstants.LEFT);
         peselT = new JTextField(TEXTFIELDCOL);
-        peselCnt.add(peselL);
-        peselCnt.add(peselT);
-
+        peselCnt.add(peselL); peselCnt.add(peselT);
         //------------------------   Sex    ------------------------
         JPanel sexCnt = new JPanel();
         JLabel sexL = new JLabel("Płeć", SwingConstants.LEFT);
         male = new JRadioButton("mężczyzna", true);
         female = new JRadioButton("kobieta", false);
         ButtonGroup group = new ButtonGroup();
-        group.add(male);
-        group.add(female);
-        sexCnt.add(sexL);
-        sexCnt.add(male); //button group is not a gui component, have to add buttons separately
-        sexCnt.add(female);
-
+        group.add(male); group.add(female);
+        sexCnt.add(sexL); sexCnt.add(male); sexCnt.add(female); //button group is not a gui component, have to add buttons separately
         //------------------------ Insurance ------------------------
         JPanel insuranceCnt = new JPanel();
         JLabel insuranceL = new JLabel("Ubezpieczenie:", SwingConstants.LEFT);
         String[] insuranceStrings = {"Brak", "NFZ", "Prywatne"};
         iBox = new JComboBox<>(insuranceStrings);
-        insuranceCnt.add(insuranceL);
-        insuranceCnt.add(iBox);
+        insuranceCnt.add(insuranceL); insuranceCnt.add(iBox);
 
         //---------------------- Add/Abort Buttons --------------------------
         JPanel aButtonCnt = new JPanel();
-
         savePatientButton = new JButton("Zapisz");
         savePatientButton.setEnabled(false);
-        savePatientButton.addActionListener((ActionEvent e) -> {
-            boolean sex;
-            sex = male.isSelected();
-            int checkAndSave;
-            if(patientTable.getSelectionModel().isSelectionEmpty()) {
-                checkAndSave = presenter.savePatient(nameT.getText(), surnameT.getText(), peselT.getText(), sex, String.valueOf(iBox.getSelectedItem()), patientTable);
-                if (checkAndSave == 0) {
-                    new ToastMessage("Dodano Pacjenta", 800, frame, Color.LIGHT_GRAY);
-                    clearAndDisableBothPanels();
-                }
-                else {
-                    AppUtils.dialogsPatientDataErrors(checkAndSave, frame);
-                }
-            }
-            else{
-                int checkAndEdit = presenter.editPatient(nameT.getText(), surnameT.getText(), peselT.getText(),
-                        sex, String.valueOf(iBox.getSelectedItem()), patientTable);
-                if(checkAndEdit == 0){
-                    new ToastMessage( "Edytowano Pacjenta", 800, frame, Color.LIGHT_GRAY);
-                    savePatientButton.setText("Zapisz");
-                    clearAndDisableBothPanels();
-                }
-                else {
-                    AppUtils.dialogsPatientDataErrors(checkAndEdit, frame);
-                }
-            }
-        });
+        savePatientButton.addActionListener((ActionEvent e) -> savePatientListener());
 
         JButton abortPatientButton = new JButton("Anuluj");
         abortPatientButton.setEnabled(false);
@@ -293,33 +242,25 @@ public class GUI extends JFrame{
         });
 
         JButton abortExamButton = new JButton("Anuluj");
-        abortExamButton.addActionListener(e-> clearExam(examPanel));
+        abortExamButton.addActionListener(e-> checkAndRetrieveExam());
         examButtons.add(saveExamButton);
         examButtons.add(abortExamButton);
 
         //---------------------------------------------------------
         //------------------------ Examination Panel main comp  ------------------------
         //---------------------------------------------------------
-        
         title = BorderFactory.createTitledBorder("Badanie");
         examPanel.setBorder(title);
-        examPanel.add(weightCnt);
-        examPanel.add(heightCnt);
-        examPanel.add(bmiCnt);
-        examPanel.add(dateCnt);
-        examPanel.add(examButtons);
+        examPanel.add(weightCnt); examPanel.add(heightCnt);examPanel.add(bmiCnt);
+        examPanel.add(dateCnt); examPanel.add(examButtons);
         AppUtils.setPanelEdit(examPanel, false);
-
 
         //---------------------------------------------------------
         //------------------------ Patient List Panel  ------------------------
         //---------------------------------------------------------
-
         //--------------------Table------------------------------
 
         patientTable = new JTable(){
-
-
                     private static final long serialVersionUID = 1L;
                     @Override
                     public Class getColumnClass(int column) {
@@ -336,38 +277,7 @@ public class GUI extends JFrame{
 
                 patientTable.setModel(defaultTableModel);
                 patientTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-
-                patientTable.getSelectionModel().addListSelectionListener(e -> {
-                    if(patientTable.getSelectedRow()!= -1) {
-                        deletePatientButton.setEnabled(true);
-                        AppUtils.setPanelEdit(examPanel, true);
-                        datePicker.getComponent(1).setEnabled(true);
-                        AppUtils.setPanelEdit(patientPanel, true);
-
-                        nameT.setText(presenter.patientVectorList.get(patientTable.getSelectedRow()).getName());
-                        surnameT.setText(presenter.patientVectorList.get(patientTable.getSelectedRow()).getSurname());
-                        peselT.setText(presenter.patientVectorList.get(patientTable.getSelectedRow()).getPesel());
-                        male.setSelected(presenter.patientVectorList.get(patientTable.getSelectedRow()).getSexBool());
-                        female.setSelected(!presenter.patientVectorList.get(patientTable.getSelectedRow()).getSexBool());
-                        iBox.setSelectedIndex(presenter.patientVectorList.get(patientTable.getSelectedRow()).getInsuranceInt());
-                        savePatientButton.setText("Edytuj");
-
-                        if (presenter.patientVectorList.get(patientTable.getSelectedRow()).isExamination()) {
-                            heightT.setText(presenter.patientVectorList.get(patientTable.getSelectedRow()).getExaminationResults().getHeight());
-                            weightT.setText(presenter.patientVectorList.get(patientTable.getSelectedRow()).getExaminationResults().getMass());
-                            bmiT.setText(presenter.patientVectorList.get(patientTable.getSelectedRow()).getExaminationResults().getBmiString());
-                            datePicker.getJFormattedTextField().setText(presenter.patientVectorList.get(patientTable.getSelectedRow()).getExaminationResults().getsDate());
-                            LocalDate localDate = presenter.patientVectorList.get(patientTable.getSelectedRow()).getExaminationResults().getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-                            int year  = localDate.getYear();
-                            int month = localDate.getMonthValue();
-                            int day   = localDate.getDayOfMonth();
-                            datePicker.getModel().setDate(year, month-1, day);
-                            datePicker.getModel().setSelected(true);
-                        }
-                        else clearExam(examPanel);
-                    }
-                    else deletePatientButton.setEnabled(false);
-                } );
+                patientTable.getSelectionModel().addListSelectionListener(e -> retrieveDataFromLibrary() );
 
             JScrollPane patientTableScroll = new JScrollPane(patientTable, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 
@@ -388,37 +298,30 @@ public class GUI extends JFrame{
             presenter.deletePatient(patientTable);
             clearAndDisableBothPanels();
         });
-
-        listButtCnt.add(addPatientButton);
-        listButtCnt.add(deletePatientButton);
-
+        listButtCnt.add(addPatientButton); listButtCnt.add(deletePatientButton);
+        //---------------------------------------------------------
+        //------------------------ Patient List main comp  ------------------------
+        //---------------------------------------------------------
         JPanel listPanel = new JPanel();
         title = BorderFactory.createTitledBorder("Lista Pacjentów");
         listPanel.setBorder(title);
-
-        listPanel.add(patientTableScroll);
-        listPanel.add(listButtCnt);
-
-        JPanel basePanel = new JPanel();
-        basePanel.setLayout(new BoxLayout(basePanel, BoxLayout.LINE_AXIS));
-        add(basePanel);
-        frame.setContentPane(basePanel);
+        listPanel.add(patientTableScroll);listPanel.add(listButtCnt);
+        listPanel.setLayout(new BoxLayout(listPanel, BoxLayout.PAGE_AXIS));
+        //------------------- final panel & frame setting --------------------------
 
         patientPanel.setLayout(new BoxLayout(patientPanel, BoxLayout.PAGE_AXIS));
         examPanel.setLayout(new BoxLayout(examPanel, BoxLayout.PAGE_AXIS));
 
-        JPanel leftPanel = new JPanel();
-        leftPanel.add(patientPanel);
-        leftPanel.add(examPanel);
-
+        JPanel leftPanel = new JPanel(); //aditional panel to store ex an patient panel
+        leftPanel.add(patientPanel); leftPanel.add(examPanel);
         leftPanel.setAlignmentX(1f);
         leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.PAGE_AXIS));
 
-        listPanel.setLayout(new BoxLayout(listPanel, BoxLayout.PAGE_AXIS));
-
-        basePanel.add(leftPanel);
-        basePanel.add(listPanel);
-
+        JPanel basePanel = new JPanel();
+        basePanel.setLayout(new BoxLayout(basePanel, BoxLayout.LINE_AXIS));
+        add(basePanel);
+        basePanel.add(leftPanel); basePanel.add(listPanel);
+        frame.setContentPane(basePanel);
         frame.setJMenuBar(menuBar);
         frame.setSize(750,550);
         frame.setLocationRelativeTo(null);
@@ -450,5 +353,62 @@ public class GUI extends JFrame{
         clearPatient(patientPanel);
         clearExam(examPanel);
         datePicker.getComponent(1).setEnabled(false);
+    }
+
+    private void savePatientListener(){
+        if(patientTable.getSelectionModel().isSelectionEmpty()) {
+            int checkAndSave = presenter.savePatient(nameT.getText(), surnameT.getText(), peselT.getText(), male.isSelected(),
+                    String.valueOf(iBox.getSelectedItem()), patientTable);
+            if (checkAndSave == 0) {
+                new ToastMessage("Dodano Pacjenta", 800, frame, Color.LIGHT_GRAY);
+                clearAndDisableBothPanels(); return;
+            }
+            AppUtils.dialogsPatientDataErrors(checkAndSave, frame);
+            return;
+        }
+            int checkAndEdit = presenter.editPatient(nameT.getText(), surnameT.getText(), peselT.getText(),
+                    male.isSelected(), String.valueOf(iBox.getSelectedItem()), patientTable);
+            if(checkAndEdit == 0){
+                new ToastMessage( "Edytowano Pacjenta", 800, frame, Color.LIGHT_GRAY);
+                savePatientButton.setText("Zapisz");
+                clearAndDisableBothPanels();
+                return;
+            }
+            AppUtils.dialogsPatientDataErrors(checkAndEdit, frame);
+    }
+
+    private void retrieveDataFromLibrary(){
+        if(!patientTable.getSelectionModel().isSelectionEmpty()) { //when row selected
+            deletePatientButton.setEnabled(true);
+            AppUtils.setPanelEdit(examPanel, true);
+            datePicker.getComponent(1).setEnabled(true);
+            AppUtils.setPanelEdit(patientPanel, true);
+
+            nameT.setText(presenter.patientVectorList.get(patientTable.getSelectedRow()).getName());
+            surnameT.setText(presenter.patientVectorList.get(patientTable.getSelectedRow()).getSurname());
+            peselT.setText(presenter.patientVectorList.get(patientTable.getSelectedRow()).getPesel());
+            male.setSelected(presenter.patientVectorList.get(patientTable.getSelectedRow()).getSexBool());
+            female.setSelected(!presenter.patientVectorList.get(patientTable.getSelectedRow()).getSexBool());
+            iBox.setSelectedIndex(presenter.patientVectorList.get(patientTable.getSelectedRow()).getInsuranceInt());
+            savePatientButton.setText("Edytuj");
+            checkAndRetrieveExam();
+        }
+        else deletePatientButton.setEnabled(false);
+
+    }
+    private void checkAndRetrieveExam(){
+        if (presenter.patientVectorList.get(patientTable.getSelectedRow()).isExamination()) {
+            heightT.setText(presenter.patientVectorList.get(patientTable.getSelectedRow()).getExaminationResults().getHeight());
+            weightT.setText(presenter.patientVectorList.get(patientTable.getSelectedRow()).getExaminationResults().getMass());
+            bmiT.setText(presenter.patientVectorList.get(patientTable.getSelectedRow()).getExaminationResults().getBmiString());
+            datePicker.getJFormattedTextField().setText(presenter.patientVectorList.get(patientTable.getSelectedRow()).getExaminationResults().getsDate());
+            LocalDate localDate = presenter.patientVectorList.get(patientTable.getSelectedRow()).getExaminationResults().getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+            int year  = localDate.getYear();
+            int month = localDate.getMonthValue();
+            int day   = localDate.getDayOfMonth();
+            datePicker.getModel().setDate(year, month-1, day);
+            datePicker.getModel().setSelected(true);
+        }
+        else clearExam(examPanel);
     }
 }
